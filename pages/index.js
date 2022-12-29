d14a9229738d682de03c76890404cf3b74f9a86e
stonyhenge <121643776+stonyhenge@users.noreply.github.com>
2022-12-29 17:44:07 -0500

    Update index.js
    Testing

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,12 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Le site de Maxime" />
+        <h1> 2022 </h1>
+        <ul>
+           <li> <a href="https://photos.google.com/share/AF1QipNFqNDPqvtJ4dZOus7Nlbt4a0To0Sj4cIAqWR69dJkb_BteHTzOHJHV29EdB2rSLg?key=WVNQM0hUYTU5QkVOY25CMlZxVExuNnZDcHRTQ0J3"> Album 1 </a></li>
+           <li> <a href="https://photos.google.com/share/AF1QipOYZjgKT3OOgJTaVaqqgymcchbYiOuQOlYewwD7oUz-5yLPBWPs_ryZWQAh8CL37g?key=dHh6MEdhMUtIakVzSGpCTmVYTlhsZHM0VjkzeVRB"> Album 2 </a></li>
+        </ul>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
