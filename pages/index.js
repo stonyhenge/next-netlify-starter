66baa697692b97e87d3c91190f920b68eb119242
stonyhenge <121643776+stonyhenge@users.noreply.github.com>
2022-12-29 20:59:05 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,10 @@ export default function Home() {
         <div>
            <h1> 2020 </h1>
            <ul>
+              <li> <a https://photos.app.goo.gl/uKRmYo1679h2DHPq9"> 2020-07-11 - Fête de Suzanne et Marie-Andrée </a></li>
               <li> <a href="https://photos.app.goo.gl/JCAvZMeqnQApiUzS7"> 2020-06-13 - Fête de Charles-Olivier </a></li>
+    
+    
            </ul>
         </div>
     
